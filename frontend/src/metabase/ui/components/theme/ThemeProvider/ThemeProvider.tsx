140a8c947045ec67c6e043f602de3c47d7f3dc50
Phoomparin Mano <phoomparin@gmail.com>
2025-04-18 00:28:38 +0700

    set MantineProvider.cssVariablesSelector to host-root

--- frontend/src/metabase/ui/components/theme/ThemeProvider/ThemeProvider.tsx
+++ frontend/src/metabase/ui/components/theme/ThemeProvider/ThemeProvider.tsx
@@ -73,6 +73,7 @@ export const ThemeProvider = (props: ThemeProviderProps) => {
       theme={theme}
       getStyleNonce={() => window.MetabaseNonce ?? "metabase"}
       classNamesPrefix="mb-mantine"
+      cssVariablesSelector=":host, :root"
       {...props.mantineProviderProps}
     >
       <_CompatibilityEmotionThemeProvider theme={theme}>
