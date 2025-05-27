cb7c10d97493381b9177a36153b9ceac2f6f8d81
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-26 20:10:50 -0400

    Add new helper

---
+++ e2e/support/helpers/api/createDashboardWithParameters.ts
@@ -0,0 +1,45 @@
+import type { LocalFieldReference } from "metabase-types/api";
+
+import { visitDashboard } from "../e2e-misc-helpers";
+
+import type { DashboardDetails } from "./createDashboard";
+import type { StructuredQuestionDetails } from "./createQuestion";
+import { createQuestionAndDashboard } from "./createQuestionAndDashboard";
+import { updateDashboardCards } from "./updateDashboardCards";
+
+export function createDashboardWithParameters(
+  questionDetails: StructuredQuestionDetails,
+  targetField: LocalFieldReference,
+  parameters: DashboardDetails["parameters"],
+  { dashboardDetails = {} }: { dashboardDetails?: DashboardDetails } = {},
+  options: { visitDashboard?: boolean } = {},
+) {
+  options.visitDashboard ??= true;
+
+  return createQuestionAndDashboard({
+    questionDetails,
+    dashboardDetails: {
+      ...dashboardDetails,
+      parameters,
+    },
+  }).then(({ body: { dashboard_id }, questionId }) => {
+    updateDashboardCards({
+      dashboard_id,
+      cards: [
+        {
+          card_id: questionId,
+          parameter_mappings: parameters?.map((parameter) => ({
+            parameter_id: parameter.id,
+            card_id: questionId,
+            target: ["dimension", targetField],
+          })),
+        },
+      ],
+    });
+
+    if (options.visitDashboard) {
+      visitDashboard(dashboard_id);
+    }
+    return cy.wrap(dashboard_id);
+  });
+}
